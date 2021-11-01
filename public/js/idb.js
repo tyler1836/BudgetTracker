525a87612fe2e7dc49942da14f452bf1074cf1d3
unknown <tyler.r.roll@gmail.com>
2021-11-01 02:56:11 -0600

    finished project

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,28 +2,67 @@ let db;
 
 const request = indexedDB.open('budget', 1);
 
-request.onupgradeneeded = function (event){
+request.onupgradeneeded = function (event) {
     const db = event.target.result;
 
-    db.createObjectStore('expenses', {autoIncrement: true})
+    db.createObjectStore('new_expense', { autoIncrement: true })
 };
 
-request.onsuccess = function(event){
+request.onsuccess = function (event) {
     db = event.target.result;
 
-    if(navigator.online){
-        uploadExpenses();
+    if (navigator.online) {
+        checkDatabase();
     }
 };
 
-request.onerror = function(event){
+request.onerror = function (event) {
     console.log(event.target.errorCode)
 };
 
-function saveRecord(transaction){
+function saveRecord(record) {
     const transaction = db.transaction(['new_expense'], 'readwrite');
 
     const expenseObjectStore = transaction.objectStore('new_expense');
 
-    expenseObjectStore.add(transaction);
+    expenseObjectStore.add(record);
 }
+
+function checkDatabase() {
+    const transaction = db.transaction(['new_expense'], 'readwrite');
+
+    const expenseObjectStore = transaction.objectStore('new_expense');
+
+    const getAll = expenseObjectStore.getAll();
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            console.log(JSON.stringify(getAll.result))
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => {
+                    console.log(response)
+                    return response.json()
+                })
+                .then((serverResponse) => {
+                    if(serverResponse.message){
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_expense'], 'readwrite');
+
+                    const expenseObjectStore = transaction.objectStore('new_expense');
+
+                    expenseObjectStore.clear();
+
+                    alert('All saved transactions have been submitted!')
+                })
+                .catch(err => console.log(err));
+        }
+    }
+};
+window.addEventListener('online', checkDatabase)
--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,15 @@ const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
 router.post("/api/transaction", ({body}, res) => {
+  console.log('api transaction')
+  console.log(body)
   Transaction.create(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
     .catch(err => {
       res.status(404).json(err);
+      console.log(err)
     });
 });
 
